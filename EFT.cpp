cf0e231c54d47f2ef80341a1aa036980e0cd9dd2
Sowmittro Singha <soumitrasingha0070@gmail.com>
2026-07-20 14:36:08 +0600

    Update EFT.cpp

--- EFT.cpp
+++ EFT.cpp
@@ -1,57 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct activity
+void activitySelection(vector<string> &act, vector<int> &st, vector<int> &end, int n)
 {
-    char name[10];
-    int st;
-    int ft;
-};
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(end[j] > end[j + 1])
+            {
+                swap(end[j], end[j + 1]);
+                swap(st[j], st[j + 1]);
+                swap(act[j], act[j + 1]);
+            }
+        }
+    }
+
+    vector<string> selected;
+    selected.push_back(act[0]);
+
+    int count = 1;
+    int lastFinish = end[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(st[i] >= lastFinish)
+        {
+            selected.push_back(act[i]);
+            count++;
+            lastFinish = end[i];
+        }
+    }
+
+    cout << count << endl;
+    cout << "Selected activities:" << endl;
+
+    for(int i = 0; i < selected.size(); i++)
+    {
+        cout << selected[i] << " ";
+    }
+    cout << endl;
+}
 
 int main()
 {
     int n;
     cin >> n;
 
-    struct activity act[n];
+    vector<string> act(n);
+    vector<int> st(n), end(n);
 
     for(int i = 0; i < n; i++)
     {
-        cin >> act[i].name >> act[i].st >> act[i].ft;
+        cin >> act[i] >> st[i] >> end[i];
     }
-
-    for(int i = 0; i < n - 1; i++)
-    {
-        for(int j = 0; j < n - i - 1; j++)
-        {
-            if(act[j].ft > act[j + 1].ft)
-            {
-                swap(act[j], act[j + 1]);
-            }
-        }
-    }
-
-    vector<string> selec_act;
-    selec_act.push_back(act[0].name);
-    int count = 1;
-    int last_ft = act[0].ft;
-
-    for(int i = 1; i < n; i++)
-    {
-        if(act[i].st >= last_ft)
-        {
-            selec_act.push_back(act[i].name);
-            count++;
-            last_ft = act[i].ft;
-        }
-    }
-
-    cout << count << endl;
-    cout << "Selected activitis: " << endl;
-    for(int i = 0; i < selec_act.size(); i++)
-    {
-        cout << selec_act[i] << " ";
-    }
-    cout << endl;
+    activitySelection(act, st, end, n);
     return 0;
 }
